637280773dddcfd5016129804a49f323860638b7
TaekedeHaan <taeke.dehaan@live.nl>
2018-05-14 12:48:00 +0200

    Stabilization sort of working in cart, still need to debug some stuff

--- arduino/control_servo/control_servo.ino
+++ arduino/control_servo/control_servo.ino
@@ -6,23 +6,24 @@
 #include <Servo.h>              // servo
 
 /* Assign a unique ID to the sensors */
-Adafruit_9DOF                 dof   = Adafruit_9DOF();
+// Adafruit_9DOF                 dof   = Adafruit_9DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 
 Servo myservo;  // create servo object to control a servo
 
 /* init pins*/
-int sensorPin = A0;    // select the input pin for the potentiometer
+byte sensorPin = A0;      // PWM pin
 int servoPin = 3;
 
-double posGoal = 90;    // variable to store the servo position
+double a = 0.5;
+
 double aServo;
 double velGoal = 0;
 double vel;
 
-double sensorValue;
+double receiverValue;
 double error;
 double errorI = 0;
 double errorD = 0;
@@ -30,12 +31,24 @@ double errorPrev = 0;
 double errorTot;
 double scaling;
 
+/* steering */
+const double receiveDef = 1437;  // [us]
+const double receiveMin = 972;   // [us]
+const double receiveMax = 1970;  // [us]
+
+const double steerAngleMin = 45; //[deg]
+const double steerAngleMax = 135; //[deg]
+const double steerAngleDef = 90; //[deg]
+
+double posGoal = steerAngleDef;    // variable to store the servo position
+double posGoalPrev = posGoal;
+
 /* CONTROLLER CONSTANTS*/
 double gamma = 0.99;
 double alpha = 0.5;
-double kP = 0.01; //0.001;
-double kI = 0.5; //0.05;
-double kD = 0.0001; //0.00005;
+double kP = 2; //0.01;
+double kI = 0.0; //0.5;
+double kD = 0.0000; //0.0001;
 
 /* Timer variables */
 unsigned long t = 0;
@@ -44,7 +57,7 @@ unsigned long tPrintPrev = 0;
 unsigned long tBlinkPrev = 0;
 
 /* init frequencies */
-const long dtControl = 5;
+const long dtControl = 200;
 const long dtPrint = 200;
 const long dtBlink = 100;
 //const long dtRead = 10;
@@ -60,20 +73,44 @@ int ledMode = LOW;
     @brief  Initialises all the sensors used by this example
 */
 /**************************************************************************/
-void initSensors()
+void displaySensorDetails(void)
 {
-  if(!accel.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-    while(1);
-  }
-  if(!mag.begin())
-  {
-    // There was a problem detecting the LSM303 ... check your connections
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while(1); 
-  }
+  sensor_t sensor;
+  
+  accel.getSensor(&sensor);
+  Serial.println(F("----------- ACCELEROMETER ----------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" m/s^2"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+
+  gyro.getSensor(&sensor);
+  Serial.println(F("------------- GYROSCOPE -----------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" rad/s"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" rad/s"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" rad/s"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  
+  mag.getSensor(&sensor);
+  Serial.println(F("----------- MAGNETOMETER -----------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" uT"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" uT"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" uT"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+
+  delay(500);
 }
 
 /**************************************************************************/
@@ -84,18 +121,37 @@ void initSensors()
 void setup(void)
 {
   Serial.begin(115200);
-  Serial.println(F("Control servo")); Serial.println("");
   
   /* Initialise the sensors */
-  initSensors();
+  Serial.println(F("====Initializing sensors====")); Serial.println("");
+  
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+  if(!mag.begin())
+  {
+    // There was a problem detecting the LSM303 ... check your connections
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
 
+  displaySensorDetails();
+  Serial.println(F("Complete!")); Serial.println("");
+  
   /* Initialise the servo */
+  Serial.println(F("====Initializing sensors====")); Serial.println("");
+  
   myservo.attach(servoPin);  // attaches the servo on pin 3 to the servo object
   aServo = posGoal;
   myservo.write(aServo);
   delay(1000);
 
   pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(sensorPin, INPUT);
 }
 
 /**************************************************************************/
@@ -118,39 +174,58 @@ void loop(void)
       }
     }
 
-    sensorValue = analogRead(sensorPin);
-    posGoal = (sensorValue/1024) * 180;
+    /* read receiver */
+    receiverValue = pulseIn(sensorPin, HIGH); //[us] read pwm pin
+    
+    if (abs(receiverValue - receiveDef) < 50){
+      receiverValue = receiveDef;
+    }
+    
+    posGoal = (receiverValue - receiveDef) * (steerAngleMax - steerAngleMin)/(receiveMax - receiveMin) + steerAngleDef;
+
+    posGoal = a * posGoal + (1-a) * posGoalPrev;
+    posGoalPrev = posGoal;
+    
+    // sensorValue = analogRead(sensorPin);  // [-] read analog pin
+    // posGoal = (sensorValue/1024) * 180;   // [deg] convert to degrees 
 
     /* updatre timing */
     jitter = t - tControlPrev - dtControl;  //[ms]
-    tControlPrev = t;                //[ms]
+    tControlPrev = t;                       //[ms]
 
     /* read gyro */
-    sensors_event_t event; //
-    gyro.getEvent(&event);
-    vel = event.gyro.z;
+    sensors_event_t event;  // create sensor event
+    gyro.getEvent(&event);  // get gyro sensor event
+    vel = event.gyro.z;     // extract velocity data around z axis.
+
+    // filter velocity
+    if (abs(vel) < 2.0){
+      vel = 0;      
+    }
 
     /* compute controler */
-    error = velGoal - vel; //[deg/s]
-    errorI = error * dtControl * 0.001 + gamma * errorI; //[deg]
-    errorD = (1 - alpha) * (error - errorPrev)/(dtControl * 0.001) + alpha * errorD; //[deg/s^2]
-    errorTot = kP * error + kI * errorI + kD * errorD;
+    error = velGoal - vel;                                                            //[deg/s]
+    errorI = error * dtControl * 0.001 + gamma * errorI;                              //[deg]
+    errorD = (1 - alpha) * (error - errorPrev)/(dtControl * 0.001) + alpha * errorD;  //[deg/s^2]
+    errorTot = kP * error + kI * errorI + kD * errorD;                                //[-]
 
-    scaling = -abs(posGoal/90 - 1) + 1;
-    errorTot = scaling * errorTot;
-    /*
+    /* apply scaling */
+    scaling = -pow(abs(posGoal/90 - 1), 0.5) + 1;   // [-] get scaling factor for error value
+    errorTot = scaling * errorTot;        // [-] scale total error
+    
+    /* set error to 0 when outside certain input range
     if (posGoal > 100 || posGoal < 80){
       errorTot = 0;
     } 
     */
     
     /* determine new setpoint */
-    aServo = posGoal - errorTot;
-    if (aServo < 0){
-      aServo = 0;
+    aServo = posGoal + errorTot;
+    if (aServo < steerAngleMin){ // limit minimum angle
+      aServo = steerAngleMin;
     }
-    else if (aServo > 180){
-      aServo = 180;
+    else if (aServo > steerAngleMax){ // limit maximum angle
+      aServo = steerAngleMax;
     }
 
     errorPrev = error;  
@@ -160,31 +235,36 @@ void loop(void)
   if (debug){
       if ((t - tPrintPrev) > dtPrint){
         tPrintPrev = t;
-        Serial.print(F("vel: "));
-        Serial.print(vel);
-        Serial.print("\t");              // prints a tab
-
-        Serial.print(F("vel goal: "));
-        Serial.print(velGoal);
-        Serial.print("\t");              // prints a tab
-
-        Serial.print(F("vel error: "));
-        Serial.print(velGoal);
+        if (false){
+          Serial.print(F("vel: "));
+          Serial.print(vel);
+          Serial.print("\t");              // prints a tab
+  
+          Serial.print(F("vel goal: "));
+          Serial.print(velGoal);
+          Serial.print("\t");              // prints a tab
+  
+          Serial.print(F("vel error: "));
+          Serial.print(velGoal);
+          Serial.print("\t");              // prints a tab
+          
+          Serial.print(F("I error: "));
+          Serial.print(errorI);
+          Serial.print("\t");              // prints a tab
+          
+          Serial.print(F("D error: "));
+          Serial.print(errorD);
+          Serial.print("\t");              // prints a tab
+          
+          Serial.print(F("tot error: "));
+          Serial.print(errorTot);
+          Serial.print("\t");              // prints a tab
+        }
+        Serial.print(F("receiver: "));
+        Serial.print(receiverValue);
         Serial.print("\t");              // prints a tab
         
-        Serial.print(F("I error: "));
-        Serial.print(errorI);
-        Serial.print("\t");              // prints a tab
-        
-        Serial.print(F("D error: "));
-        Serial.print(errorD);
-        Serial.print("\t");              // prints a tab
-        
-        Serial.print(F("tot error: "));
-        Serial.print(errorTot);
-        Serial.print("\t");              // prints a tab
-
-        Serial.print(F("set point angle: "));
+        Serial.print(F("angle servo: "));
         Serial.print(aServo);
         Serial.print("\t");              // prints a tab
 
--- simulation/data/mech_heavy.m
+++ simulation/data/mech_heavy.m
@@ -5,11 +5,11 @@ clc
 label = 'mech_heavy';
 
 % mechanical
-r = 0.075/2; %[m] wheel redius
+r = 0.080/2; %[m] wheel redius
 m = 3.0; %[kg]
 
 % lijkt ergens tussen de 1:9 en 1:12 te liggen
-n = 50/14 * 2.5/1; % [-] transmission
+n = 50/18 * 2.5/1; % [-] transmission
 mu = 0.15; % [-] losses in total mechanical power train
 
 save(label)
