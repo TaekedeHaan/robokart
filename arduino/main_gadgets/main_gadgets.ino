22e316163d114d469b17fe65a5cdf447b6e0d8e8
TaekedeHaan <taeke.dehaan@live.nl>
2018-05-26 19:07:00 +0200

    leds seem to be working, ready for outside test

--- arduino/main_gadgets/main_gadgets.ino
+++ arduino/main_gadgets/main_gadgets.ino
@@ -2,13 +2,21 @@
 #define RPIN 5
 #define GPIN 6
 #define BPIN 3
-#define ESCPIN A0      // pin from receiver
+#define ESCPIN A0     // pin from receiver
+#define LFPIN 7 // left forward
+#define RFPIN 8 // right forward
+#define LBPIN 9 // left back
+#define RBPIN 10 // right back
+#define SERVOPIN 11 // servo pin
 
 // set prefquencies
 #define DTPRINT 200   // [ms] for printing
-#define DTLED 50     // [ms] for updating LEDs
-#define DTREAD 100    // [ms] for reading velocity value
+#define DTLED 50      // [ms] for updating LEDs
+#define DTREAD 20    // [ms] for reading velocity value
 #define DTBLINK 200
+#define DTWEAPON 50   // [ms]
+#define DTTHROTTLE 2000 // [ms]
+#define DTACTION 100 // [ms]
 
 // recever output PWM width
 #define AVG 1500
@@ -24,13 +32,25 @@
 
 # define DEBUG true
 
+boolean releaseWeapon = false;
+
 unsigned long t;
 unsigned long tPrintPrev = 0;
 unsigned long tReadVelPrev = 0;
 unsigned long tLedPrev = 0;
 unsigned long tBlinkPrev = 0;
+unsigned long tActionPrev = 0;
+unsigned long tThrottlePrev = 0;
 
 int brakeCount = 0; // brake counter
+int throttleCount = 0; // throttle counter
+int throttleThreshold = 3; // threshold
+
+// init 
+int lf = LOW;
+int rf = LOW;
+uint8_t lb = 0;
+uint8_t rb = 0;
 
 // init RGB
 double r;
@@ -48,7 +68,16 @@ void setup() {
   pinMode(GPIN, OUTPUT);
   pinMode(BPIN, OUTPUT);
   pinMode(ESCPIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  /* led pins*/
+  pinMode(LFPIN, OUTPUT);
+  pinMode(RFPIN, OUTPUT);
+  pinMode(LBPIN, OUTPUT);
+  pinMode(RBPIN, OUTPUT);  
 
+  pinMode(SERVOPIN, OUTPUT);
+  
   Serial.println(F("Complete setup!")); Serial.println("");
 }
  
@@ -63,12 +92,13 @@ void loop() {
     /* read receiver, pulse length from 1ms (zero throttle) to 2ms (full throttle) */
     velWidth = pulseIn(ESCPIN, HIGH); //[us] read pwm pin
 
+    /* filter velocity in */
     if (abs(velWidth - AVG) < MARGIN){
       velWidth = AVG;
     }
 
     /* count braking */
-    if (prevVelWidth >= AVG){
+    if (prevVelWidth >= AVG){ // if we were previously pressing gass
 
       /* If we are braking and previously weren't */
       if (velWidth < AVG){
@@ -76,7 +106,24 @@ void loop() {
       }
     }
 
-    /* if we are floowing that gass pedal again reset the braking counter */
+    /* count gass */
+    if (prevVelWidth <= AVG){ // if we were previously not pressing gass
+
+      /* If we are braking and previously weren't */
+      if (velWidth > AVG){
+
+        /* If within timer increment throttle count, otherwise reset */
+        if (t - tThrottlePrev < DTTHROTTLE){
+          throttleCount = throttleCount + 1;
+        }
+        else {
+          tThrottlePrev = t;
+          throttleCount = 0;
+        }
+      }
+    }
+
+    /* if we are pressing the gass pedal reset the braking counter */
     if (velWidth > AVG) {
       brakeCount = 0;
     }
@@ -89,9 +136,9 @@ void loop() {
 
     /* color function */
     if (brakeCount == 0){
-      r = OUTMAX + pow(SLOPE * (velWidth - MAX),LEDINT);
-      g = OUTMAX - pow(SLOPE * abs(-velWidth + (AVG + MAX)/2),LEDINT);
-      b = OUTMAX + pow(SLOPE * (-velWidth + AVG),LEDINT);
+      r = OUTMAX + SLOPE * (velWidth - MAX);
+      g = OUTMAX - SLOPE * abs(-velWidth + (AVG + MAX)/2);
+      b = OUTMAX + SLOPE * (-velWidth + AVG);
     }
     else {
       r = OUTMAX - SLOPE * (velWidth - MIN);
@@ -123,21 +170,49 @@ void loop() {
       }
     }
 
+    /* LED's */
+    lf = HIGH;
+    rf = HIGH;
+    rb = 255;
+    lb = 255;
+
     /* output new LED values */
     analogWrite(RPIN, r);
     analogWrite(GPIN, g);
     analogWrite(BPIN, b);
+
+    /* update LED's */
+    digitalWrite(LFPIN, rf);
+    digitalWrite(RFPIN, rf);
+    analogWrite(LBPIN, lb);
+    analogWrite(RBPIN, rb);
+  }
+
+  if (t - tActionPrev > DTACTION){
+
+    t = tActionPrev;
+    
+    /* wehn sufficient amount of trottles set realease weapon flag */
+    if (throttleCount >= throttleThreshold) {
+      releaseWeapon = true;
+    }
+
+   /* release weapon*/
+    if (releaseWeapon) {
+      digitalWrite(LED_BUILTIN, releaseWeapon);
+    }
   }
   
   /* print */
   if (DEBUG) {
     if (t - tPrintPrev > DTPRINT){
       tPrintPrev = t;
-      
+
+      /*
       Serial.print(F("esc width: "));
       Serial.print(velWidth);   
       Serial.print(F(" [ns]"));
-
+      */
       Serial.print(F("r: "));
       Serial.print(r);   
       Serial.print("\t");              // prints a tab
@@ -153,7 +228,7 @@ void loop() {
       Serial.print(F("braking counter: "));
       Serial.print(brakeCount);   
       Serial.print("\t");              // prints a tab
-
+      
       Serial.println("");
     }
   }
