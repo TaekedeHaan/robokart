36f78922a316beefe51d8e33bb8109f81fdf36ca
Taeke de Haan <Taeke.dehaan@live.nl>
2019-09-22 13:13:12 +0200

    Added release machnism functionality, control loop became too slow!

--- arduino/2019/Hunnie/main/main.ino
+++ arduino/2019/Hunnie/main/main.ino
@@ -12,13 +12,35 @@ Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
 
-Servo myservo;  // create servo object to control a servo
+Servo steerServo;  // create servo object to control the steering servo
+Servo releaseServo;  // create servo object to control the release servo
 
 /* init pins*/
-// byte velocityReadPin = A2;
-// int releasePin = 6;
-byte sensorPin = A1;      //
-int servoPin = 3;         // servo pin
+#define RELEASEPIN 6
+#define STEERIN A1     //
+#define STEEROUTPIN 3     // servo pin
+#define VELOCITYIN A3  // pin which reads velocity
+
+/* receiver velocity PWM width */
+#define VELOCITY_IN_MEAN 1470
+#define VELOCITYINMAX 2000
+#define VELOCITYINMIN 1000
+#define VELOCITY_IN_MARGIN 50
+#define DTTHROTTLE 2000 // [ms]
+
+/* receiver steering PWM width */
+#define STEER_IN_MIN 972   // [us] minimum width receiving steering signal
+#define STEER_IN_MEAN 1437  // [us] default width receiving steering signal
+#define STEER_IN_MAX 1970  // [us] maximum width receiving steering signal
+#define STEER_IN_MARGIN 60
+
+/* Steering servo angles */
+#define STEERANGLEMIN 45 //[deg] minimum steering angle
+#define STEERANGLEAVG 90 //[deg] default steering angle
+#define STEERANGLEMAX 135//[deg] maximum steering angle
+
+#define RELEASE_NEUTRAL_ANGLE 180
+#define RELEASE_ACTIVATE_ANGLE 80
 
 double filterPosGoal = 1.0; // importantance curerent value
 
@@ -26,7 +48,11 @@ double aServo;
 double velGoal = 0;
 double vel;
 
-double receiverValue;
+// velocity
+double velocityIn;
+double velocityInPrev;
+
+double steerIn;
 double error;
 double errorI = 0;
 double errorD = 0;
@@ -40,15 +66,6 @@ double scaleIntenstity = 1;
 
 double velTreshold = 0.02; // below treshold vel is set to 0
 
-/* steering */
-const double RECEIVEWIDTHMIN = 972;   // [us] minimum width receiving steering signal
-const double RECEIVEWIDTHAVG = 1437;  // [us] default width receiving steering signal
-const double RECEIVEWIDTHMAX = 1970;  // [us] maximum width receiving steering signal
-
-const double STEERANGLEMIN = 45; //[deg] minimum steering angle
-const double STEERANGLEAVG = 90; //[deg] default steering angle
-const double STEERANGLEMAX = 135;//[deg] maximum steering angle
-
 /* CONTROLLER CONSTANTS*/
 const double filterP = 1;     // importantance curerent value
 const double filterI = 0.95;   // 0.75  // importantance curerent value
@@ -60,18 +77,29 @@ const double KD = 0.5; //0.5;
 double posGoal = STEERANGLEAVG;   // variable to store the servo position. initialize in middle
 double posGoalPrev = posGoal;     // variable to store previous pos goal
 
+/* */
+int brakeCount = 0; // brake counter
+int throttleCount = 0; // throttle counter
+int throttleThreshold = 3; // threshold
+
 /* Timer variables */
 unsigned long t = 0;
 unsigned long tControlPrev = 0;
 unsigned long tPrintPrev = 0;
+unsigned long tGadgetsPrev = 0;
 unsigned long tBlinkPrev = 0;
+unsigned long tThrottlePrev = 0;
 unsigned long jitter;
-// unsigned long tReadPrev;
+
+
+/* gadgets*/
+boolean releaseWeapon = false;
 
 /* init frequencies */
-const long dtControl = 20;
+const long dtControl = 30; //20;
 const long dtPrint = 500;
 const long dtBlink = 100;
+const long dtGadgets = 200;
 //const long dtRead = 10;
 
 boolean debug = true;
@@ -80,7 +108,7 @@ int ledMode = LOW;
 void displaySensorDetails(void)
 {
   sensor_t sensor;
-  
+
   accel.getSensor(&sensor);
   Serial.println(F("----------- ACCELEROMETER ----------"));
   Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
@@ -102,7 +130,7 @@ void displaySensorDetails(void)
   Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" rad/s"));
   Serial.println(F("------------------------------------"));
   Serial.println(F(""));
-  
+
   mag.getSensor(&sensor);
   Serial.println(F("----------- MAGNETOMETER -----------"));
   Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
@@ -110,7 +138,7 @@ void displaySensorDetails(void)
   Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
   Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" uT"));
   Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" uT"));
-  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" uT"));  
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" uT"));
   Serial.println(F("------------------------------------"));
   Serial.println(F(""));
 
@@ -121,43 +149,49 @@ void setup(void)
 {
   Serial.begin(115200);
   Serial.println(F("Adafruit 9DOF Tester")); Serial.println("");
-  
+
   /* Initialise the sensors */
-  if(!accel.begin())
+  if (!accel.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-    while(1);
+    while (1);
   }
-  if(!mag.begin())
+  if (!mag.begin())
   {
     /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while(1);
+    while (1);
   }
-  if(!gyro.begin())
+  if (!gyro.begin())
   {
     /* There was a problem detecting the L3GD20 ... check your connections */
     Serial.print("Ooops, no L3GD20 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    while (1);
   }
-  
+
   /* Display some basic information on this sensor */
   displaySensorDetails();
 
   Serial.println(F("Complete!")); Serial.println("");
-  
+
   /* Initialise the servo */
   Serial.println(F("====Initializing sensors====")); Serial.println("");
-  
-  myservo.attach(servoPin);  // attaches the servo on pin 3 to the servo object
+
+  /* Steer servo */
+  steerServo.attach(STEEROUTPIN);
   aServo = posGoal;
-  myservo.write(aServo);
+  steerServo.write(aServo);
+
+  /* Release servo */
+  releaseServo.attach(RELEASEPIN); 
+  releaseServo.write(RELEASE_NEUTRAL_ANGLE);
+  
   delay(1000);
 
   pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(sensorPin, INPUT);
+  pinMode(STEERIN, INPUT);
+  pinMode(VELOCITYIN, INPUT);
 }
 
 void loop(void)
@@ -165,12 +199,11 @@ void loop(void)
 
   t = millis(); // [ms]
 
-
   /* Control loop */
-  if ((t - tControlPrev) >= dtControl){
-    
+  if ((t - tControlPrev) >= dtControl) {
+
     /* Activate led when too much jitter*/
-    if ((t - tControlPrev) >= 1.5 * dtControl){
+    if ((t - tControlPrev) >= 1.5 * dtControl) {
       ledMode = HIGH;
       if (debug) {
         Serial.print(F("==Didn't reach loop=="));
@@ -178,24 +211,65 @@ void loop(void)
       }
     }
 
-    /* read receiver */
-    receiverValue = pulseIn(sensorPin, HIGH); //[us] read pwm pin
+        /* updatre timing */
+    jitter = t - tControlPrev - dtControl;  //[ms]
+    tControlPrev = t;                       //[ms]
 
-    /* filter noise */
-    if (abs(receiverValue - RECEIVEWIDTHAVG) < 50){
-      receiverValue = RECEIVEWIDTHAVG;
+    /* read receiver */
+    steerIn = pulseIn(VELOCITYIN, HIGH); //[us] read pwm pin
+    velocityIn = pulseIn(VELOCITYIN, HIGH); //[us] read pwm pin
+
+    /* filter noise on steer */
+    if (abs(steerIn - STEER_IN_MEAN) < STEER_IN_MARGIN) {
+      steerIn = STEER_IN_MEAN;
     }
 
+    /* filter noise on velocity */
+    if (abs(velocityIn - VELOCITY_IN_MEAN) < VELOCITY_IN_MARGIN) {
+      velocityIn = VELOCITY_IN_MEAN;
+    }
+
+    /* count braking */
+    if (velocityInPrev >= VELOCITY_IN_MEAN) { // if we were previously pressing gass
+
+      /* If we are braking and previously weren't */
+      if (velocityIn < VELOCITY_IN_MEAN) {
+        brakeCount = brakeCount + 1;
+      }
+    }
+
+    /* if we are pressing the gass pedal reset the braking counter */
+    if (velocityIn > VELOCITY_IN_MEAN) {
+      brakeCount = 0;
+    }
+    
+    /* count gass */
+    if (velocityInPrev <= VELOCITY_IN_MEAN) { // if we were previously not pressing gass
+
+      /* If we are braking and previously weren't */
+      if (velocityIn > (VELOCITY_IN_MEAN + 1.5*VELOCITY_IN_MARGIN) ) {
+
+        /* If within timer increment throttle count, otherwise reset */
+        if (t - tThrottlePrev < DTTHROTTLE) {
+          throttleCount = throttleCount + 1;
+        }
+      }
+    }
+
+    if (t - tThrottlePrev > DTTHROTTLE) {
+      throttleCount = 0;
+      tThrottlePrev = t;
+    }
+
+
+    velocityInPrev = velocityIn;
+
     /* convert reveiver value to desired steering angle with simple interpolation */
-    posGoal = (receiverValue - RECEIVEWIDTHAVG) * (STEERANGLEMAX - STEERANGLEMIN)/(RECEIVEWIDTHMAX - RECEIVEWIDTHMIN) + STEERANGLEAVG;
+    posGoal = (steerIn - STEER_IN_MEAN) * (STEERANGLEMAX - STEERANGLEMIN) / (STEER_IN_MAX - STEER_IN_MIN) + STEERANGLEAVG;
 
     /* fitler desired steering angle */
     posGoal = filterPosGoal * posGoal + (1 - filterPosGoal) * posGoalPrev;
     posGoalPrev = posGoal;
-
-    /* updatre timing */
-    jitter = t - tControlPrev - dtControl;  //[ms]
-    tControlPrev = t;                       //[ms]
 
     /* read gyro */
     sensors_event_t event;  // create sensor event
@@ -203,88 +277,120 @@ void loop(void)
     vel = event.gyro.z;     // [rad/s] extract velocity data around z axis.
 
     /* filter velocity */
-    if (abs(vel) <= velTreshold){
-      vel = 0;      
+    if (abs(vel) <= velTreshold) {
+      vel = 0;
     }
 
     /* compute scaling */
-    scaling = -pow(abs(posGoal/90 - 1), scaleIntenstity) + 1;   // [-] get scaling factor for error value
+    scaling = -pow(abs(posGoal / 90 - 1), scaleIntenstity) + 1; // [-] get scaling factor for error value
 
     /* when we exit a corner we dont want the PID to directly kick in */
-    if (scaling > scalingPrev){
+    if (scaling > scalingPrev) {
       scaling = scalingPrev + scalingIncrement * (scaling - scalingPrev);
     }
-    
+
     /* compute controler */
     error  = scaling * filterP * (velGoal - vel) + (1 - filterP) * error;                            //[rad/s]
     errorI = scaling * error * dtControl * 0.001 + filterI * errorI;                                //[rad]
-    errorD = scaling * filterD * (error - errorPrev)/(dtControl * 0.001) + (1 - filterD) * errorD;  //[rad/s^2]
+    errorD = scaling * filterD * (error - errorPrev) / (dtControl * 0.001) + (1 - filterD) * errorD; //[rad/s^2]
     errorTot = KP * error + KI * errorI + KD * errorD;                                    //[-]
 
     // errorTot = scaling * errorTot;                  // [-] scale total error
     errorPrev = error;
-    scalingPrev = scaling; 
-    
+    scalingPrev = scaling;
+
     /* determine new setpoint */
     aServo = posGoal + errorTot;
-    if (aServo < STEERANGLEMIN){ // limit minimum angle
+    if (aServo < STEERANGLEMIN) { // limit minimum angle
       aServo = STEERANGLEMIN;
     }
-    else if (aServo > STEERANGLEMAX){ // limit maximum angle
+    else if (aServo > STEERANGLEMAX) { // limit maximum angle
       aServo = STEERANGLEMAX;
     }
 
-    myservo.write(aServo);
+    steerServo.write(aServo);
+  }
+
+
+  /* gadgets loop */
+  if ((t - tGadgetsPrev) > dtGadgets){
+    tGadgetsPrev = t;
+    
+    if (throttleCount == throttleThreshold){
+      releaseWeapon = true;
+    }
+
+    if (releaseWeapon){
+      releaseServo.write(RELEASE_ACTIVATE_ANGLE);
+    }
   }
 
   /* prints for debugging */
-  if (debug){
-      if ((t - tPrintPrev) > dtPrint){
-        tPrintPrev = t;
+  if (debug) {
+    if ((t - tPrintPrev) > dtPrint) {
+      tPrintPrev = t;
 
+      Serial.print("Brake counter: ");
+      Serial.print(brakeCount);
+      Serial.print("\t");
+
+      Serial.print("Throttle counter: ");
+      Serial.print(throttleCount);
+      Serial.print("\t");
+
+      Serial.print("velocity PWM width: ");
+      Serial.print(velocityIn);
+      Serial.print("[us] \t");
+
+      Serial.print("Steer PWM width: ");
+      Serial.print(steerIn);
+      Serial.print("\t");              // prints a tab
+     Serial.println("");
+
+
+      if (false) {
         Serial.print(F("I error: "));
         Serial.print(errorI);
         Serial.print("\t");              // prints a tab
-        
-        if (false){  
-          Serial.print(F("vel goal: "));
-          Serial.print(velGoal);
-          Serial.print("\t");              // prints a tab
-  
-          Serial.print(F("vel error: "));
-          Serial.print(velGoal);
-          Serial.print("\t");              // prints a tab
-          
-          Serial.print(F("D error: "));
-          Serial.print(errorD);
-          Serial.print("\t");              // prints a tab
-          
-          Serial.print(F("tot error: "));
-          Serial.print(errorTot);
-          Serial.print("\t");              // prints a tab
 
-          Serial.print(F("vel: "));
-          Serial.print(vel);
-          Serial.print("\t");              // prints a tab
-          
-          Serial.print(F("receiver: "));
-          Serial.print(receiverValue);
-          Serial.print("\t");              // prints a tab
-          
-          Serial.print(F("angle servo: "));
-          Serial.print(aServo);
-          Serial.print("\t");              // prints a tab
-  
-          Serial.print(F("jitter: "));
-          Serial.print(jitter);   
-          Serial.print(F(" [ms]"));
-          Serial.println("");
-        }
+        Serial.print(F("vel goal: "));
+        Serial.print(velGoal);
+        Serial.print("\t");              // prints a tab
+
+        Serial.print(F("vel error: "));
+        Serial.print(velGoal);
+        Serial.print("\t");              // prints a tab
+
+        Serial.print(F("D error: "));
+        Serial.print(errorD);
+        Serial.print("\t");              // prints a tab
+
+        Serial.print(F("tot error: "));
+        Serial.print(errorTot);
+        Serial.print("\t");              // prints a tab
+
+        Serial.print(F("vel: "));
+        Serial.print(vel);
+        Serial.print("\t");              // prints a tab
+
+        Serial.print(F("receiver: "));
+        Serial.print(steerIn);
+        Serial.print("\t");              // prints a tab
+
+        Serial.print(F("angle servo: "));
+        Serial.print(aServo);
+        Serial.print("\t");              // prints a tab
+
+        Serial.print(F("jitter: "));
+        Serial.print(jitter);
+        Serial.print(F(" [ms]"));
+        Serial.println("");
       }
     }
+  }
 
   /* Blink LED if we do not meet desider control frequency */
-  if ((t - tBlinkPrev) > dtBlink){
+  if ((t - tBlinkPrev) > dtBlink) {
     tBlinkPrev = t;
     digitalWrite(LED_BUILTIN, ledMode);
     ledMode = LOW;
